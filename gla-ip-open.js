3f694ccf7a99178800cee75e83e471d71896ebc3
TacoScott <withorwithoutgod@gmail.com>
2015-07-08 14:55:42 -0500

    Update gla-ip-open.js

--- gla-ip-open.js
+++ gla-ip-open.js
@@ -10,13 +10,28 @@
 
 if (window.addEventListener) {
   window.addEventListener('keydown', function (e) {
-    console.log(e.keyCode);
-    //q
-    if (e.keyCode == 81) {
-      $("#login_history a.sa_ip").each(function()
-                                       {
-                                           GM_openInTab(window.location.origin + $(this).attr("href"));
-                                       });
-    }     
+      console.log(e.keyCode);
+      //q
+      if (e.keyCode == 81) {
+          $("#login_history a.sa_ip").each(function()
+                                           {
+                                               GM_openInTab(window.location.origin + $(this).attr("href"));
+                                           });
+      }
+      
+      //e
+      if (e.keyCode == 69) {
+          $("#profile a[href*='spammeradmin']").first().each(function() {
+              GM_openInTab(window.location.origin + $(this).attr("href"));
+              
+          });
+      }
+      //w
+      if (e.keyCode == 87) {
+          $("#messages div.current a[href*='superadmin']").first().each(function() {
+              GM_openInTab(window.location.origin + $(this).attr("href"));
+              
+          });
+      }
   }, true);
 }
